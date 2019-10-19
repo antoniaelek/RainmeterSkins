4a9ee6a04f9d434f1dc76aa55883d63883f4c73a
Antonia Elek <antoniaelek@hotmail.com>
2019-10-20 01:38:31 +0200

    Fetch book name.

--- GoodreadsQuotes/@Resources/Python/GetQuote.py
+++ GoodreadsQuotes/@Resources/Python/GetQuote.py
@@ -2,6 +2,9 @@ import random
 import feedparser
 import html
 import sys
+import requests
+import re
+from selectolax.parser import HTMLParser
 
 
 def get_quotes(user_id, user_name, sort=False):
@@ -42,6 +45,16 @@ def _parse_rss_page(url):
     return page_quotes
 
 
+def _parse_quote_page(url):
+    data = requests.get(url)
+    dom = HTMLParser(data.text)
+    for tag in dom.css('a.authorOrTitle'):
+        if 'href' in tag.attributes:
+            return (tag.text(), tag.attributes['href'])
+
+    return None
+
+
 def update_file(text, file):
     if text != "":
         f = open(file,"w+", encoding='utf-8')
@@ -53,18 +66,32 @@ if __name__ == '__main__':
     if len(sys.argv) == 4:
         out_folder = sys.argv[1]
         print(out_folder)
+        
         user_id = sys.argv[2]
         print(user_id)
+
         user_name = sys.argv[3]
         print(user_name)
         
         quotes = get_quotes(user_id, user_name, sort=True)
         if (len(quotes)>0):
-            quote = random.choice(quotes)
+            quote_obj = random.choice(quotes)
+            author = quote_obj[0]
+            quote = quote_obj[1]
+            link_quote = quote_obj[2]
+            link_book = "https://www.goodreads.com"
+            
+            book_obj = _parse_quote_page(link_quote)
+            if book_obj != None:
+                author = author + ", " + book_obj[0]
+                link_book = "https://www.goodreads.com" + book_obj[1]
+            
             print("Got a random quote!")
-            update_file(quote[1], out_folder+"quote.txt")
-            update_file(quote[0], out_folder+"author.txt")
-            update_file(quote[2], out_folder+"link.txt")
+
+            update_file(quote, out_folder+"quote.txt")
+            update_file(author, out_folder+"author.txt")
+            update_file(link_quote, out_folder+"link_quote.txt")
+            update_file(link_book, out_folder+"link_book.txt")
         else:
             print("No quotes found for the specified user!")
     else:
